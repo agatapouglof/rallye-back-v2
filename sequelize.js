f2f714460fa64780a6a7e10d261364c99c935cf6
jonatan nougbele <001ndj@gmail.com>
2019-02-12 13:53:45 +0000

    add  mysql port

--- sequelize.js
+++ sequelize.js
@@ -2,9 +2,10 @@ const Sequelize = require('sequelize')
 let config = require('config');
 
 
-
+console.log(config);
 const sequelizeConnect = new Sequelize(config.db.database, config.db.username, config.db.password, {
   host: config.db.host,
+  port : config.db.port,
   dialect: 'mysql',
   pool: {
     max: 100,
